8066974a89c642690259572d335080c91104881e
Harrison Bui <hsbui@hotmail.com>
2017-04-05 20:11:56 -0700

    add filter bar

--- search_app/App/Containers/TrackedItems.js
+++ search_app/App/Containers/TrackedItems.js
@@ -130,3 +130,106 @@ export default class TrackedItems extends React.Component {
 
 // renderHeader={() => <Header />}   search bar
 // renderSectionHeader={(sectionData) => <SectionHeader {...sectionData} />}   Letter seperator like phone book
+
+
+
+
+
+
+
+// import React, {Component, PropTypes} from 'react';
+// import {
+//     View,
+//     Text,
+//     StyleSheet
+// } from 'react-native';
+// import { Select, SelectTextBox, Option, OptionList } from 'react-native-multi-select';
+//
+//
+// export default class LocationDropDown extends Component {
+//     static propTypes = {};
+//
+//     cities = [
+//         'Babolsar',
+//         'Sari',
+//         'Babol',
+//         'Qaemshahr',
+//         'Gorgan',
+//         'Tehran',
+//         'ali abad',
+//         'gonbad',
+//         'mashhad',
+//         'esfehan',
+//         'shiraz',
+//         'kerman',
+//         'ilam',
+//         'sanandaj',
+//         'mahshahr',
+//         'behshar',
+//         'tonekabon'
+//     ];
+//
+//     state = {
+//         selectedItem: [],
+//         text: "",
+//         displayOptionList: false
+//     };
+//
+//     updateText = text => {
+//         this.setState({text});
+//     };
+//
+//     addItem = item => {
+//         this.setState({selectedItem: [...this.state.selectedItem, item]})
+//     };
+//
+//     removeItem = removedItem => {
+//         this.setState({
+//             selectedItem: this.state.selectedItem.filter(item => {
+//                 if (item._id !== removedItem._id)
+//                     return item;
+//             })
+//         });
+//     };
+//
+//     render() {
+//         return (
+//             <Select style={styles.select}>
+//                 <SelectTextBox
+//                     selectedItem={this.state.selectedItem}
+//                     placeholder={"شهر خود را انتخاب کنید."}
+//                     onPressOut={(removedItem) => this.setState({
+//                         selectedItem: this.state.selectedItem.filter(text => {
+//                             if (text !== removedItem)
+//                                 return text;
+//                         })
+//                     })}
+//                     onTextInputFocus={() => this.setState({displayOptionList: true})}
+//                     onTextInputLoosFocus={() => this.setState({displayOptionList: false})}
+//                     onSelectTextBoxChanged={event => this.updateText(event.nativeEvent.text)}
+//                 />
+//                 <OptionList
+//                     text={this.state.text}
+//                     items={this.cities}
+//                     display={this.state.displayOptionList}
+//                     onAnItemSelected={item => this.addItem(item)}
+//                     removeItem={item => this.removeItem(item)}>
+//                     {this.cities.map((city, index) => <Option
+//                         onPress={item => this.setState({selectedItem: [...this.state.selectedItem, item]})}
+//                         key={index}
+//                         value={{_id: Math.random()}}>
+//                         {city}
+//                     </Option>)}
+//                 </OptionList>
+//             </Select>
+//         );
+//     }
+// }
+//  
+// const styles = StyleSheet.create({
+//     select: {
+//         backgroundColor: '#6A85B1',
+//         width: 300,
+//     }
+// });
+//
